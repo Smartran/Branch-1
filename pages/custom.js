d4b6679c68640366da7a7a9ff855e2e99487601d
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2022-11-06 21:01:04 +0530

    Update custom.js

--- pages/custom.js
+++ pages/custom.js
@@ -28,9 +28,9 @@ export default function custom() {
                 <input className="px-4 h-12 my-2 border border-1 border-gray-600 rounded-lg outline-blue-600 bg-white text-black" type="text" placeholder="Amount (USD)" />
                 <input className="px-4 h-12 my-2 border border-1 border-gray-600 rounded-lg outline-blue-600 bg-white text-black" type="text" placeholder="Enter Wallet Address" />
                 <input className="px-4 h-12 my-2 border border-1 border-gray-600 rounded-lg outline-blue-600 bg-white text-black" type="password" placeholder="Add a note" />
-                <button className="bg-slate-900 text-purple-500 my-2 py-2 rounded-md font-bold hover:bg-slate-700">Upload Files</button>
+                <button className="bg-slate-900 text-purple-500 my-2 py-2 rounded-md font-bold hover:bg-slate-700 text-xl">Upload Files</button>
                 <hr className="my-2" />
-                <button className=" bg-slate-900 text-purple-500 my-2 py-2 px-4 mx-auto rounded-md font-bold w-fit">Mint NFT</button>
+                <button className=" bg-slate-900 text-purple-500 my-2 py-2 px-4 mx-auto rounded-md font-bold w-fit text-xl">Mint NFT</button>
                 {/* <span className="-bottom-10 text-sm text-black font-bold">
                     NOTE : NFT will be minted to the entered wallet addresses !</span> */}
                     </div></div>
