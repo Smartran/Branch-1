5e46c5ed7a16bb20c8c3619ad187837639ce01c1
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2022-11-06 16:55:51 +0530

    Update custom.js

--- pages/custom.js
+++ pages/custom.js
@@ -19,12 +19,12 @@ export default function custom() {
             <div className='grid grid-cols-2'>
 
             <div className=' mx-16 my-10 text-3xl text-neon-100 font-bold'>
-                Did nt Find any <br></br>NFT that meets your<br></br> Requirement?
+                Didnt Find any <br></br>NFT that meets your<br></br> Requirement?
             <br></br>Then Make one <br/>of your Own !
                 <br/>
             </div>
-    
-            <div className=" flex flex-col bg-neon-100  rounded-xl p-8 w-1/4 mx-4 my-4 col-span-4">
+            <div> 
+            <div className=" flex flex-col bg-neon-100  rounded-xl p-8 w-1/2 mx-20 my-2 ">
                 <input className="px-4 h-12 my-2 border border-1 border-gray-600 rounded-lg outline-blue-600 bg-white text-black" type="text" placeholder="Amount (USD)" />
                 <input className="px-4 h-12 my-2 border border-1 border-gray-600 rounded-lg outline-blue-600 bg-white text-black" type="text" placeholder="Enter Wallet Address" />
                 <input className="px-4 h-12 my-2 border border-1 border-gray-600 rounded-lg outline-blue-600 bg-white text-black" type="password" placeholder="Add a note" />
@@ -32,7 +32,7 @@ export default function custom() {
                 <hr className="my-2" />
                 <button className=" bg-green-500 text-white my-2 py-2 px-4 mx-auto rounded-md font-bold w-fit hover:bg-green-700">Mint NFT</button>
                 <span className="-bottom-10 text-sm text-black">
-                    NOTE : NFT will be minted to the entered wallet addresses !</span></div>
+                    NOTE : NFT will be minted to the entered wallet addresses !</span></div></div>
             </div>
         
         <Footer />
