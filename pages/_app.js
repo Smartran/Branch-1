68e4d012c6b47aa5310f0d2aec345ec5be50c3e5
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2022-11-07 16:35:40 +0530

    main

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,10 @@
 import '../styles/globals.css'
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThirdwebProvider
-      desiredChainId={1}
-      authConfig={{
-        // Set this to your domain to prevent signature malleability attacks.
-        domain: "example.com",
-        authUrl: "/api/auth",
-      }}
-    >
+    
       <Component {...pageProps} />
-    </ThirdwebProvider>
   );
 }
 export default MyApp
